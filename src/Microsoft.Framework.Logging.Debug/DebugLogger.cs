36884a853ac38b927fc5b5ec8149f365c3c7875c
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2015-07-01 10:28:36 -0700

    Doc comments for debug logger

--- src/Microsoft.Framework.Logging.Debug/DebugLogger.cs
+++ src/Microsoft.Framework.Logging.Debug/DebugLogger.cs
@@ -2,43 +2,70 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections;
 using System.Diagnostics;
-using System.Text;
 
 namespace Microsoft.Framework.Logging.Debug
 {
+    /// <summary>
+    /// A logger that writes messages in the debug output window only when a debugger is attached.
+    /// </summary>
     public class DebugLogger : ILogger
     {
         private readonly Func<string, LogLevel, bool> _filter;
         private readonly string _name;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugLogger"/> class.
+        /// </summary>
+        /// <param name="name">The name of the logger.</param>
         public DebugLogger(string name)
             : this(name, filter: null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugLogger"/> class.
+        /// </summary>
+        /// <param name="name">The name of the logger.</param>
+        /// <param name="filter">The function used to filter events based on the log level.</param>
         public DebugLogger(string name, Func<string, LogLevel, bool> filter)
         {
             _name = string.IsNullOrEmpty(name) ? nameof(DebugLogger) : name;
-            _filter = filter ?? ((category, logLevel) => true);
+            _filter = filter;
         }
 
+
+        /// <summary>
+        /// Begins a logical operation scope.
+        /// </summary>
+        /// <param name="state">The identifier for the scope.</param>
+        /// <returns>An IDisposable that ends the logical operation scope on dispose.</returns>
         public IDisposable BeginScopeImpl(object state)
         {
             return null;
         }
 
+        /// <summary>
+        /// Checks if the given <see cref="LogLevel"/> is enabled.
+        /// </summary>
+        /// <param name="logLevel">The log level to be checked.</param>
+        /// <returns>True if enabled, false otherwise.</returns>
         public bool IsEnabled(LogLevel logLevel)
         {
-            if (!Debugger.IsAttached)
-            {
-                return false;
-            }
-
-            return _filter(_name, logLevel);
+            // If the filter is null, everything is enabled
+            // unless the debugger is not attached
+            return Debugger.IsAttached &&
+                (_filter == null || _filter(_name, logLevel));
         }
 
+        /// <summary>
+        /// Aggregates most logging patterns to a single method.
+        /// </summary>
+        /// <param name="logLevel">The level of the event being logged.</param>
+        /// <param name="eventId">The id of the event being logged.</param>
+        /// <param name="state">The state of the event being logged.</param>
+        /// <param name="exception">The exception associated with the event being logged.</param>
+        /// <param name="formatter">A formatter that will be used to format the event being logged.</param>
         public void Log(LogLevel logLevel, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
         {
             if (!IsEnabled(logLevel))
@@ -69,7 +96,7 @@ namespace Microsoft.Framework.Logging.Debug
             {
                 return;
             }
-
+            
             message = $"{ logLevel }: {message}";
             System.Diagnostics.Debug.WriteLine(message, _name);
         }
--- src/Microsoft.Framework.Logging.Debug/DebugLoggerProvider.cs
+++ src/Microsoft.Framework.Logging.Debug/DebugLoggerProvider.cs
@@ -5,15 +5,27 @@ using System;
 
 namespace Microsoft.Framework.Logging.Debug
 {
+    /// <summary>
+    /// The provider for the <see cref="DebugLogger"/>.
+    /// </summary>
     public class DebugLoggerProvider : ILoggerProvider
     {
         private readonly Func<string, LogLevel, bool> _filter;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugLoggerProvider"/> class.
+        /// </summary>
+        /// <param name="filter">The function used to filter events based on the log level.</param>
         public DebugLoggerProvider(Func<string, LogLevel, bool> filter)
         {
             _filter = filter;
         }
 
+        /// <summary>
+        /// Create a new instance of the <see cref="DebugLogger"/> class.
+        /// </summary>
+        /// <param name="name">The logger name.</param>
+        /// <returns>A new instance of the logger.</returns>
         public ILogger CreateLogger(string name)
         {
             return new DebugLogger(name, _filter);
